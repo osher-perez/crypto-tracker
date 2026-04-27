139dbbf4eeb3d2958a66bde8e0fbaad9f5593385
Oshir <osherp96@gmail.com>
2026-04-27 08:53:28 +0300

    feat: add amount state and price calculation logic

--- app/coin/[id]/Converter.tsx
+++ app/coin/[id]/Converter.tsx
@@ -2,6 +2,8 @@
 
 import {useState} from "react";
 
+const [amount , setAmount ] = useState(1);
+const totalPrice = amount * price ;
 export default function Converter({price , symbole}:{price : string , symbole : string}){
     return <div> מחשבון שלי </div>
 }
